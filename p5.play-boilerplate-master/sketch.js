7df38226960f9edd5a9a756cbc56a5873b00cf33
prathiksha1516 <71827736+prathiksha1516@users.noreply.github.com>
2021-01-03 20:46:15 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,47 @@
+var hr,min,sec
+var hrAngle,minAngle,secAngle
+
+
+
+
+
+
+function setup() {
+  createCanvas(800,800);
+angleMode(DEGREES)
+}
+
+function draw() {
+  background(255,255,255);  
+translate(400,400)
+rotate (-90)
+ hr=hour()
+ min=minute ()
+ sec=second()
+ secAngle=map(sec,0,60,0,360)
+ minAngle=map(min,0,60,0,369)
+ hrAngle=map(hr%12,0,12,0,360)
+ push ()
+ rotate(secAngle)
+ stroke ("red")
+ strokeWeight(6)
+ line (0,0,200,0)
+ pop ()
+ push ()
+ rotate(minAngle)
+ stroke ("blue")
+ strokeWeight(6)
+ line (0,0,150,0)
+ pop ()
+ push ()
+ rotate(hrAngle)
+ stroke ("green")
+ strokeWeight(6)
+ line (0,0,100,0)
+ pop ()
+ 
+
+
+
+
+}
